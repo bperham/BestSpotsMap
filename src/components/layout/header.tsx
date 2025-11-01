51fb58e671c83f97b4ae558df0478c49c8e12315
perhammaps <perhammaps@gmail.com>
2025-11-01 06:10:00 +0000

    header buffer

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -47,7 +47,7 @@ export function Header({
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
+      <div className="container flex h-14 max-w-screen-2xl items-center px-2">
         <div className="mr-4 flex items-center">
           <button onClick={handleHomeClick} className="flex items-center space-x-2 focus:outline-none">
             <Logo className="h-10 w-10" />
